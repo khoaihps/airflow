c895ca8c14e40b97e83b8ca809e51a9f9f3dd5fb
KhoaDoan <khoadoan2472003@gmail.com>
2025-06-16 11:15:13 +0700

    add new dags

---
+++ dags/scraper/coindesk_news.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def sample_task():
+    print("This is a sample task")
+
+with DAG(
+    dag_id="scrape_coindesk_news",
+    schedule_interval="@daily",
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    start = EmptyOperator(task_id="start")
+
+    task1 = PythonOperator(
+        task_id="task1",
+        python_callable=sample_task,
+    )
+
+    start >> task1
